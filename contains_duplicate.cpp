4385af25b7dd1a402a83c6e64d49806c8c63113b
Shivansh <shivansh.gamelab@gmail.com>
2026-07-06 10:42:20 +0530

    third code more logic

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,40 +1,82 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main(){
+//     int n;
+
+//     cout<<"enter number of elements";
+//     cin>>n;
+
+//     vector<int>nums(n);
+
+//     cout << "enter number of elements";
+
+//     for(int i = 0 ; i<n ; i++){
+//         cin >> nums[i];
+//     }
+
+//     sort(nums.begin(),nums.end());
+
+//     bool found = false;
+
+//     for(int i = 1 ; i<n ; i++){
+//         if(nums[i] == nums[i-1]){
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if(found){
+//         cout << "contains duplicate";
+//     }
+//     else{
+//         cout << "no duplicate found";
+//     }
+
+//     return 0;
+// }
+
+########
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
 
-    cout<<"enter number of elements";
-    cin>>n;
+    cout << "Enter number of elements: ";
+    cin >> n;
 
-    vector<int>nums(n);
+    vector<int> nums(n);
 
-    cout << "enter number of elements";
-
-    for(int i = 0 ; i<n ; i++){
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+    {
         cin >> nums[i];
     }
 
-    sort(nums.begin(),nums.end());
+    unordered_set<int> seen;
 
-    bool found = false;
-
-    for(int i = 1 ; i<n ; i++){
-        if(nums[i] == nums[i-1]){
-            found = true;
-            break;
+    for (int num : nums)
+    {
+        if (seen.find(num) != seen.end())
+        {
+            cout << "Contains Duplicate" << endl;
+            return 0;
         }
+
+        seen.insert(num);
     }
 
-    if(found){
-        cout << "contains duplicate";
-    }
-    else{
-        cout << "no duplicate found";
-    }
+    cout << "No Duplicate Found" << endl;
 
     return 0;
 }
+
