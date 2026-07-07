d826a58e31edfbbd73d2ff67c048a0660f3d47d2
Shivansh <shivansh.gamelab@gmail.com>
2026-07-08 00:56:19 +0530

    seventh code

---
+++ product_of_array_except_self.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    vector<int> prefix(n);
+    vector<int> suffix(n);
+    vector<int> answer(n);
+
+    prefix[0] = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
+    }
+
+    suffix[n - 1] = 1;
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        answer[i] = prefix[i] * suffix[i];
+    }
+
+    cout << "Answer:\n";
+
+    for (int num : answer)
+    {
+        cout << num << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
